77e49a4130d0cb140d1be6bb2177de94ae9c727b
Christian Visintin <christian.visintin@veeso.dev>
2026-03-01 09:54:52 +0100

    refactor: remove duplicated IC database engine module

--- crates/ic-dbms/ic-dbms-canister/src/dbms/integrity.rs
+++
@@ -1,8 +0,0 @@
-//! This module exposes all the integrity validators for the DBMS.
-
-mod common;
-mod insert;
-mod update;
-
-pub use self::insert::InsertIntegrityValidator;
-pub use self::update::UpdateIntegrityValidator;
