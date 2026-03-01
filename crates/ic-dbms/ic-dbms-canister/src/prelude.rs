7fe3ccb1c94707127693132b2c255d987c685e84
Christian Visintin <christian.visintin@veeso.dev>
2026-03-01 09:55:49 +0100

    refactor: update ic-dbms-canister prelude to re-export from wasm-dbms

--- crates/ic-dbms/ic-dbms-canister/src/prelude.rs
+++ crates/ic-dbms/ic-dbms-canister/src/prelude.rs
@@ -3,9 +3,11 @@
 pub use ic_dbms_api::prelude::*;
 pub use ic_dbms_macros::DbmsCanister;
 
-pub use crate::dbms::IcDbmsDatabase;
-pub use crate::dbms::integrity::{InsertIntegrityValidator, UpdateIntegrityValidator};
-pub use crate::dbms::referenced_tables::get_referenced_tables;
-pub use crate::dbms::schema::DatabaseSchema;
-pub use crate::dbms::transaction::TRANSACTION_SESSION;
-pub use crate::memory::{ACL, MEMORY_MANAGER, SCHEMA_REGISTRY, SchemaRegistry};
+// Re-export from wasm-dbms instead of deleted IC modules
+pub use wasm_dbms::prelude::{
+    DatabaseSchema, DbmsContext, InsertIntegrityValidator, UpdateIntegrityValidator,
+    WasmDbmsDatabase, get_referenced_tables,
+};
+pub use wasm_dbms::transaction::session::TransactionSession;
+
+pub use crate::memory::{DBMS_CONTEXT, IcMemoryProvider};
