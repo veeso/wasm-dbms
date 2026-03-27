d1f335ea61a8eec0078de241b724f95fbd7d00ff
Christian Visintin <christian.visintin@veeso.dev>
2026-03-27 10:31:16 +0100

    build: update dependencies

--- crates/ic-dbms/integration-tests/pocket-ic-tests/src/pocket_ic/env.rs
+++ crates/ic-dbms/integration-tests/pocket-ic-tests/src/pocket_ic/env.rs
@@ -7,7 +7,7 @@ use flate2::read::GzDecoder;
 pub use pocket_ic::PocketIcBuilder;
 use tokio::sync::OnceCell;
 
-const POCKET_IC_SERVER_VERSION: &str = "12.0.0";
+const POCKET_IC_SERVER_VERSION: &str = "13.0.0";
 const POCKET_IC_BIN: &str = "POCKET_IC_BIN";
 
 /// Returns the pocket-ic client.
